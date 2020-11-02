efdab478033c65065b8d83512c867cfbd2e353c9
Ilya Kireev <ilyakireev53@mail.ru>
2020-11-02 16:59:33 +0300

    Added test script

--- test.sh
+++ test.sh
@@ -1,37 +1,61 @@
 gcc -o main  main.c
 
+sudo apt install btrfs-progs
+
 truncate --size=1G FAT.txt
 sudo mkfs.fat FAT.txt
 mkdir fat
 sudo mount FAT.txt fat
-echo -e "\nFAT file system mounted successfully!"
-echo "Let's check it..."
-./main fat
-sudo umount fat
-rmdir fat
-rm FAT.txt
-echo ""
 
 truncate --size=1G exFAT.txt
 sudo mkfs.exfat exFAT.txt
 mkdir exFAT
 sudo mount exFAT.txt exFAT
-echo -e "\nexFAT file system mounted successfully!"
-echo "Let's check it..."
-./main exFAT
-sudo umount exFAT
-rmdir exFAT
-rm exFAT.txt
-echo ""
 
 truncate --size=1G ext4.txt
 sudo mkfs.ext4 ext4.txt
 mkdir ext4
 sudo mount ext4.txt ext4
-echo -e "\next4 file system mounted successfully!"
-echo "Let's check it..."
+
+truncate --size=1G btrfs.txt
+sudo mkfs.btrfs btrfs.txt
+mkdir btrfs
+sudo mount btrfs.txt btrfs
+
+truncate --size=1G ntfs.txt
+blockDevice=`sudo losetup -f`
+sudo losetup $blockDevice ntfs.txt
+sudo mkntfs $blockDevice
+mkdir ntfs
+sudo mount $blockDevice ntfs
+
+echo -e "\nFAT file system check:"
+./main fat
+sudo umount fat
+rmdir fat
+rm FAT.txt
+
+echo -e "\nexFAT file system check:"
+./main exFAT
+sudo umount exFAT
+rmdir exFAT
+rm exFAT.txt
+
+echo -e "\next4 file system check:"
 ./main ext4
 sudo umount ext4
 rmdir ext4
 rm ext4.txt
-echo ""
+
+echo -e "\nbtrfs file system check:"
+./main btrfs
+sudo umount btrfs
+rmdir btrfs
+rm btrfs.txt
+
+echo -e "\nntfs file system check:"
+./main ntfs
+sudo umount ntfs
+sudo losetup -d $blockDevice
+rmdir ntfs
+rm ntfs.txt
