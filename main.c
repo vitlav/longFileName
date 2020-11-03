a7e5e8c84f4c7de40ba167e20fcc6fcfe7e1f5d6
Ilya Kireev <ilyakireev53@mail.ru>
2020-11-04 00:02:22 +0300

    Fix indents

--- main.c
+++ main.c
@@ -20,33 +20,34 @@ printf("Unknown OS!")
 #define BTRFS_SUPER_MAGIC     0x9123683e
 
 const char *fsType2str(long type) {
-	static struct fsname {
-		long type;
-		const char *name;
-	} table[] = {
-		{ EXT4_SUPER_MAGIC, "EXT4" },
-		{ MSDOS_SUPER_MAGIC, "FAT" },
-		{ FUSE_SUPER_MAGIC, "exFAT" },
-		{ NTFS_SB_MAGIC, "NTFS" },
-		{ HPFS_SUPER_MAGIC, "HPFS" },
-		{ BTRFS_SUPER_MAGIC, "BTRFS" },
-		{ 0, NULL },
-	};
-	static char unknown[100];
-	
-	for (int i = 0; table[i].type != 0; i++)
-		if (table[i].type == type)
-			return table[i].name;
+    static struct fsname {
+        long type;
+        const char *name;
+    } table[] = {
+        { EXT4_SUPER_MAGIC, "EXT4" },
+        { MSDOS_SUPER_MAGIC, "FAT" },
+        { FUSE_SUPER_MAGIC, "exFAT" },
+        { NTFS_SB_MAGIC, "NTFS" },
+        { HPFS_SUPER_MAGIC, "HPFS" },
+        { BTRFS_SUPER_MAGIC, "BTRFS" },
+        { 0, NULL },
+    };
+    static char unknown[100];
+    
+    for (int i = 0; table[i].type != 0; i++)
+        if (table[i].type == type)
+            return table[i].name;
 
-	sprintf(unknown, "unknown type:");
-	return unknown;
+    sprintf(unknown, "unknown type:");
+    return unknown;
 }
 
 int isFileExistsAccess(int argc, char *argv[]) {
-	if (argc != 2) {
-		printf("Wrong number of parameters! Enter only the path to the file or directory!\n");
-		return 1;
-	}
+    if (argc != 2) {
+        printf("Wrong number of parameters! Enter only the path to the file or directory!\n");
+        return 1;
+        
+    }
     if (access(argv[1], F_OK) != 0) {
 		printf("No such file or directory: %s\n", argv[1]);
     	return 1;
@@ -55,12 +56,13 @@ int isFileExistsAccess(int argc, char *argv[]) {
 }
 
 int main(int argc, char *argv[]) {
+    
+    if (isFileExistsAccess(argc, argv)) {
+        return 0;
+        
+    }
 
-	if (isFileExistsAccess(argc, argv)) {
-		return 0;
-	}
-
-	struct statfs fs;
+    struct statfs fs;
     char *file = argv[1];
     int result = statfs(file, & fs);
     if (result != 0) {
@@ -69,12 +71,12 @@ int main(int argc, char *argv[]) {
         return errno;
     }
 
-	#if __APPLE__
-    	printf("tf_type: %s\n", fs.f_fstypename);
-	#elif __linux__
-		printf("tf_type: %s %#lx\n", fsType2str(fs.f_type), fs.f_type);
-		printf("tf_namelen: %ld\n", fs.f_namelen);
-	#endif
+    #if __APPLE__
+        printf("tf_type: %s\n", fs.f_fstypename);
+    #elif __linux__
+        printf("tf_type: %s %#lx\n", fsType2str(fs.f_type), fs.f_type);
+        printf("tf_namelen: %ld\n", fs.f_namelen);
+    #endif
 
     return 0;
 }
